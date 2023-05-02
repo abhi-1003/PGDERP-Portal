30fedad33935327ec99e44e81919cfdf061cf374
pt_3002 <prerna_tulsiani@hotmail.com>
2023-05-02 22:19:21 +0530

    Login and Register Page added in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import UserLogin from "./pages/UserLogin";
 import FormComponent from './components/formComponent';
+import UserRegister from "./pages/UserRegister";
 
 function App() {
   return(
-    <FormComponent/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserLogin/>} exact></Route>
+        <Route path="/register" element={<UserRegister/>}></Route>
+        <Route path="/form" element={<FormComponent/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
