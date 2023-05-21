5a2b28aaabe398da0e83dfa94cf8475b5ea46ba0
abhi-1003 <abhikurule24@gmail.com>
2023-05-21 22:46:11 +0530

    name and color changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -48,8 +48,8 @@ export default function NavBar(props) {
   return (
     <nav className="NavbarItems" style={{ width: "100%" }}>
       <Helmet>
-        <title>COEP - PhD admission Portal</title>
-        <meta name="title" content="COEP PhD Admission Portal"></meta>
+        <title>COEP - PGDERP admission Portal</title>
+        <meta name="title" content="COEP PGDERP Admission Portal"></meta>
       </Helmet>
 
       <ul className="ul-class">
@@ -65,9 +65,9 @@ export default function NavBar(props) {
         <li className="li-class">
           <a href="/">
             <h1 id="big-screen">
-              PhD Admission Portal - College of Engineering, Pune
+              PGDERP Admission Portal - COEP Technological University
             </h1>
-            <h1 id="small-screen">PhD Admission Portal - COEP</h1>
+            <h1 id="small-screen">PGDERP Admission Portal - COEP Tech</h1>
           </a>
         </li>
 
